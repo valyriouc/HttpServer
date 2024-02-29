e21ef8d6af778d3f53b7acb06ca916c5d17f32a6
Valacor <valyrious.informatics@gmail.com>
2024-02-29 17:34:53 +0100

    Implement builder interface for protocol platforms

--- Server.Core/Application/ServerPlatform.cs
+++ Server.Core/Application/ServerPlatform.cs
@@ -45,7 +45,11 @@ public class HttpProtocolPlatform : IProtocolPlatform
 
 public interface IProtocolPlatformBuilder<TPlatform> : IBuilder<TPlatform>
 {
+    public IProtocolPlatformBuilder<TPlatform> WithMethods(HashSet<string> methods);
 
+    public IProtocolPlatformBuilder<TPlatform> WithVersions(HashSet<Version> versions);
+
+    public IProtocolPlatformBuilder<TPlatform> WithHeaders(Dictionary<string, string[]> headers);
 }
 
 public class ServerPlatformBuilder<THandler> : IBuilder<IServerPlatform>
