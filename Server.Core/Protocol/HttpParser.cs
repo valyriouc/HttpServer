50ed8e7e3f172d922933ebf8fd54ce8ecc5db4d9
Valacor <valyrious.informatics@gmail.com>
2024-03-04 10:30:05 +0100

    Rework the http parser with a new interface

--- Server.Core/Protocol/HttpParser.cs
+++ Server.Core/Protocol/HttpParser.cs
@@ -5,70 +5,93 @@ using System.Text;
 
 namespace Server.Core.Protocol;
 
-internal class HttpParser
+public enum ParserState
 {
-    private enum ParsingState
+    NotStarted = 0,
+    Parsing = 1,
+    Finished = 3
+}
+
+public interface ILoggeble
+{
+    public ILogger Logger { get; }
+}
+
+public interface IParser<TNode>
+{
+    public ParserState State { get; set; }
+
+    public IEnumerable<TNode> Parse();
+}
+
+public enum HttpPart
+{
+    Method = 0,
+    Url = 1,
+    Version = 2,
+    Header = 3,
+    Body = 4
+}
+
+public struct HttpNode
+{
+    public HttpPart Part { get; }
+
+    public byte[] Content { get; }
+
+    public HttpNode(HttpPart part, byte[] content)
     {
-        NotStarted,
-        Method = 0,
-        Url = 1,
-        Version = 2,
-        Headers = 3,
-        Body = 4,
-        Finished = 5
+        Part = part;
+        Content = content;
     }
+}
 
+internal class HttpParser : IParser<HttpNode>, ILoggeble
+{
     protected readonly ReadOnlyMemory<byte> data;
-
-    private readonly ILogger logger;
-    private readonly HttpRequestBuilder builder;
-
     private int ptr;
-    private ParsingState state;
+
+    public ILogger Logger { get; }
+
+    public ParserState State { get; set; }
 
     public HttpParser(ReadOnlyMemory<byte> data, ILogger logger)
     {
         this.data = data;
         ptr = 0;
-        state = ParsingState.NotStarted;
-        this.logger = logger;
-
-        builder = new HttpRequestBuilder();
+        State = ParserState.NotStarted;
+        Logger = logger;
     }
 
-    public HttpRequest? Parse()
+    public IEnumerable<HttpNode> Parse()
     {
         if (data.IsEmpty)
         {
-            return null;
+            throw new HttpParserException("Invalid parser data!");
         }
 
-        if (state != ParsingState.NotStarted)
+        if (State != ParserState.NotStarted)
         {
             throw new HttpParserException("Invalid parsing state!");
         }
 
-        ParseMethod();
-        ParseUrl();
-        ParseVersion();
+        State = ParserState.Parsing;
+
+        yield return ParseMethod();
+        yield return ParseUrl();
+        yield return ParseVersion();
+        
         EnsureNewLine();
 
-        if (state != ParsingState.Version)
+        foreach (HttpNode header in ParseHeaders())
         {
-            throw new HttpParserException("Invalid parser state!");
+            yield return header;
         }
 
-        do
+        if (State == ParserState.Parsing)
         {
-            ParseHeader();
-        } while (state == ParsingState.Headers);
-
-        if (state == ParsingState.Body)
-        {
-            ReadBody();
+            yield return ReadBody();
         }
-
-        return builder.Build();
     }
 
     private void EnsureSpace()
@@ -102,64 +125,55 @@ internal class HttpParser
         throw new HttpParserException("Expected a new line!");
     }
 
-    protected internal void ParseMethod()
+    protected HttpNode ParseMethod()
     {
         switch (data.Span[0])
         {
             case 0x47:
                 {
-                    builder.WithMethod(HttpMethod.Get);
-                    ptr += 3;
-                    logger.Info("Read get message!");
+                    Logger.Info("Read get method!");
+                    return new HttpNode(HttpPart.Method, "GET");
                 }
-                break;
             case 0x50:
                 {
                     if (data.Span[1] == 0x4f)
                     {
-                        builder.WithMethod(HttpMethod.Post);
-                        ptr += 4;
-                        logger.Info("Read post message!");
+                        Logger.Info("Read post method!");
+                        return new HttpNode(HttpPart.Method, "POST");
                     }
                     else
                     {
                         throw new NotImplementedException("Http method is not supported!");
                     }
                 }
-                break;
             default:
                 throw new NotImplementedException("Http method is not supported!");
-                break;
         }
-
-        state = ParsingState.Method;
     }
 
-    protected internal void ParseUrl()
+    protected HttpNode ParseUrl()
     {
-        if (state != ParsingState.Method)
+        if (State != ParserState.Parsing)
         {
             throw new HttpParserException("Invalid parser state!");
         }
 
         EnsureSpace();
 
-        logger.Warn(ptr.ToString());
-
         ReadOnlySpan<byte> slice = data.Span.Slice(ptr);
 
         int index = slice.IndexOf((byte)0x20);
 
-        builder.WithUrl(Encoding.UTF8.GetString(slice[0..index]));
-
         ptr += index;
 
-        state = ParsingState.Url;
+        return new HttpNode(
+            HttpPart.Url, 
+            slice[0..index].ToArray());
     }
 
-    protected internal void ParseVersion()
+    protected HttpNode ParseVersion()
     {
-        if (state != ParsingState.Url)
+        if (State != ParserState.Parsing)
         {
             throw new HttpParserException("Invalid parser state!");
         }
@@ -180,77 +194,69 @@ internal class HttpParser
                 "Malformed head line");
         }
 
-        state = ParsingState.Version;
-
         if (line.Length == 7)
         {
+            ptr += line.Length - 1;
+
             if (line[5] == 0x31)
             {
-                builder.WithVersion(HttpVersion.Version10);
+                return new HttpNode(HttpPart.Version, Encoding.UTF8.GetBytes("HTTP1"));
             }
             if (line[5] == 0x32)
             {
-                builder.WithVersion(HttpVersion.Version20);
-
+                return new HttpNode(HttpPart.Version, Encoding.UTF8.GetBytes("HTTP2"));
             }
             if (line[5] == 0x33)
             {
-                builder.WithVersion(HttpVersion.Version30);
+                return new HttpNode(HttpPart.Version, Encoding.UTF8.GetBytes("HTTP3"));
             }
-
-            ptr += line.Length - 1;
-            return;
         }
 
         if (line.Length == 9)
         {
-            builder.WithVersion(HttpVersion.Version11);
             ptr += line.Length - 1;
-            return;
+            return new HttpNode(HttpPart.Version, Encoding.UTF8.GetBytes("HTTP1.1"));
         }
 
         throw HttpException.InternalServerError("Error when parsing the http version!");
     }
 
-    protected internal void ParseHeader()
+    protected IEnumerable<HttpNode> ParseHeaders()
     {
-        state = ParsingState.Headers;
-
-        ReadOnlySpan<byte> slice = data.Span.Slice(ptr);
-
-        if (slice.IsEmpty)
+        if (State != ParserState.Parsing)
         {
-            state = ParsingState.Finished;
-            return;
+            throw new HttpParserException(
+                "Invalid parser state!");
         }
 
-        if (slice[0] == (byte)'\r' && slice[1] == (byte)'\n')
+        while (true)
         {
-            state = ParsingState.Body;
-            return;
+            ReadOnlySpan<byte> slice = data.Span.Slice(ptr);
+
+            if (slice.IsEmpty)
+            {
+                State = ParserState.Finished;
+                break;
+            }
+
+            if (slice[0] == (byte)'\r' && slice[1] == (byte)'\n')
+            {
+                break;
+            }
+
+            int length = slice.IndexOf((byte)'\r') + 1;
+            ptr += length - 1;
+
+            EnsureNewLine();
+
+            yield return new HttpNode(HttpPart.Header, slice[0..length].ToArray());
         }
-
-        int nameLength = slice.IndexOf((byte)0x3a);
-        string name = Encoding.UTF8.GetString(slice[0..nameLength]);
-        ptr += nameLength + 1;
-
-        EnsureSpace();
-
-        slice = data.Span.Slice(ptr);
-
-        int valueLength = slice.IndexOf((byte)'\r') + 1;
-        string value = Encoding.UTF8.GetString(slice[0..valueLength]);
-        ptr += valueLength - 1;
-
-        EnsureNewLine();
-
-        builder.WithHeader(name.Trim(), value.Trim());
     }
 
-    protected void ReadBody()
+    protected HttpNode ReadBody()
     {
         ReadOnlySpan<byte> slice = data.Span.Slice(ptr);
         int firstNull = slice.IndexOf((byte)0x00);
-        builder.WithBody(slice[..firstNull]);
+        return new HttpNode(HttpPart.Body, slice[..firstNull].ToArray());
     }
 }
