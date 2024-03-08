5b3ed53ef8b75b6f013657a8bfa8077a7b1e43ae
Valacor <valyrious.informatics@gmail.com>
2024-03-08 21:11:13 +0100

    Rework the part enums

---
+++ Server.Core/Protocol/HttpParts.cs
@@ -0,0 +1,18 @@
+﻿namespace Server.Core.Protocol;
+
+internal enum HttpRequestParts
+{
+    Method = 0,
+    Url = 1,
+    Version = 2,
+    Header = 3,
+    Body = 4
+}
+
+internal enum HttpResponseParts
+{
+    Version = 0,
+    StatusCode = 1,
+    Header = 2,
+    Body = 3
+}
