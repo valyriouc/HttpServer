54406e86e36fe5213c3e9ddab1105fded880d353
Valacor <valyrious.informatics@gmail.com>
2024-03-08 21:11:32 +0100

    Naive implementation of the generator

---
+++ Server.Core/Protocol/HttpGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Vectorize.Logging;
+using Vectorize.Server.Protocol;
+
+namespace Server.Http.Protocol;
+
+// TODO: This can be a generic generator
+internal sealed class HttpGenerator : ILoggable
+{
+    public ILogger Logger { get; }
+
+    public HttpGenerator(ILogger logger)
+    {
+        Logger = logger;
+    }
+
+    public ReadOnlyMemory<byte> Generate(IEnumerable<ParserNode> input)
+    {
+        List<byte> bytes = new List<byte>();
+
+        foreach (ParserNode node in input)
+        {
+            bytes.AddRange(node.Content);
+        }
+
+        return bytes.ToArray();
+    }
+}
