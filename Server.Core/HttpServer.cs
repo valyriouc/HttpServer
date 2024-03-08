2dee64817e945f9a68b7407a1a563b4d4c203535
Valacor <valyrious.informatics@gmail.com>
2024-03-08 19:34:53 +0100

    Http implementation following the new interfaces

---
+++ Server.Core/HttpServer.cs
@@ -0,0 +1,73 @@
+﻿using Server.Core.Http;
+using Server.Core.Protocol;
+using Server.Generic;
+using Vectorize.Logging;
+using Vectorize.Server;
+using Vectorize.Server.Handles;
+
+namespace Server.Http;
+
+internal interface IProtocolPlatform<TRequest, TResponse>
+    where TRequest : IFromParsing<TRequest>
+    where TResponse : IToParsingInput
+{
+    public Task<TResponse> HandleRequestAsync(TRequest request, CancellationToken token);
+}
+
+internal class HttpPlatform : IProtocolPlatform<HttpRequest, HttpResponse>
+{
+    public Task<HttpResponse> HandleRequestAsync(HttpRequest request, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class HttpGenerator : ILoggable
+{
+    public ILogger Logger { get; }
+
+    public HttpGenerator(ILogger logger)
+    {
+        Logger = logger;
+    }
+
+    public ReadOnlyMemory<byte> Generate(IEnumerable<ParserNode> input)
+    {
+
+        return Array.Empty<byte>();
+    }
+}
+
+internal class HttpServer : IServerConsumer
+{
+    public ILogger Logger { get; set; }
+
+    public HttpParser Parser { get; }
+
+    public HttpGenerator Generator { get; set; }
+
+    public HttpPlatform Platform { get; set;}
+
+    public HttpServer(HttpPlatform platform, ILogger logger)
+    {
+        Logger = logger;
+        Platform = platform;
+        Parser = new HttpParser(Logger);
+        Generator = new HttpGenerator(Logger);
+    }
+
+    public async Task ConsumeAsync(IByteHandle handle, CancellationToken token)
+    {
+        Parser.Feed(await handle.ReadBytesAsync(token));
+
+        IEnumerable<ParserNode> parsed = Parser.Parse();
+
+        HttpResponse res = await Platform.HandleRequestAsync(
+            await HttpRequest.GetFromParsingAsync(parsed, token), 
+            token);
+
+        ReadOnlyMemory<byte> generated = Generator.Generate(await res.TransformToAsync(token));
+
+        await handle.WriteBytesAsync(generated, token);
+    }
+}
